d53ff6fad6f75eb8c9a1774a617d5e0602927727
Marie BOUËTEL <marie.bouetel@edu.esiee.fr>
2026-02-20 10:53:42 +0100

    3 commit for section 7

--- lab/lab2/td2/scripts/tofu/live/sample-app/main.tf
+++ lab/lab2/td2/scripts/tofu/live/sample-app/main.tf
@@ -2,17 +2,25 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+variable "ami_id" {
+  type = string
+}
+
 variable "instances" {
   type = map(object({
     name          = string
     instance_type = string
     port          = number
   }))
+
+  default = {
+    app1 = { name = "sample-app-1", instance_type = "t3.micro", port = 8080 }
+    app2 = { name = "sample-app-2", instance_type = "t3.micro", port = 8080 }
+  }
 }
 
-# Instantiate module per entry in the instances map
 module "sample_app" {
-  source = "github.com/marieb240/DevOps-Labs.git//lab/lab2/td2/scripts/tofu/modules/ec2-instance""
+  source   = "github.com/marieb240/DevOps-Labs.git//lab/lab2/td2/scripts/tofu/modules/ec2-instance"
   for_each = var.instances
 
   name          = each.value.name
@@ -21,8 +29,6 @@ module "sample_app" {
   ami_id        = var.ami_id
 }
 
-output "public_ip" {
-  value = aws_instance.sample_app.public_ip
+output "public_ips" {
+  value = [for m in values(module.sample_app) : m.public_ip]
 }
-
-variable "ami_id" { type = string }
